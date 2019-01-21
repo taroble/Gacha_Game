9e5564b821376dfd67648d02f1fc4bec06f5cde3
Michael Perez <mperez980089@yahoo.com>
2019-01-21 01:46:01 -0800

    started on gamemaster and camerashakehandler

---
+++ Assets/Scripts/CameraShakeHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakeHandler : MonoBehaviour
+{
+    Vector2 homePos;
+    float shakeIntensity;
+
+    void Start()
+    {
+        homePos = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector2(homePos.x + Random.Range(-shakeIntensity, shakeIntensity), homePos.y + Random.Range(-shakeIntensity, shakeIntensity));
+    }
+}
