f227784be2c1e67293f1bc1d00de729a73b67a32
Kristi Chang <kristi.chang@csu.fullerton.edu>
2019-02-08 02:33:49 -0800

    fountain scene fixed, working

--- Assets/Scripts/CoinSilver.cs
+++ Assets/Scripts/CoinSilver.cs
@@ -5,47 +5,28 @@ using UnityEngine;
 public class CoinSilver : MonoBehaviour
 {
 	
-	bool clicked;
-	public int coinValue;
-
-	public float minLifeLength;
-	public float maxLifeLength;
-	float lifeTimerLength;
-	float lifeTimer;
-
 	[HideInInspector]
-	public CoinSpawner coinSpawner;
-
-
+	public GameMaster gameMaster;
 
 	void Start()
 	{
-		lifeTimerLength = Random.Range(minLifeLength, maxLifeLength);
-		lifeTimer = lifeTimerLength;
-
-		transform.localScale = new Vector2(0.2f, 0.2f);
-		GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
-		
-		
-		GetComponent<Renderer>().material.color = Color.cyan;
+		GetComponent<SpriteRenderer>().color = Color.cyan;
 
 	}
 
 	void Update()
 	{
-		if (!clicked)
-		{
-			GetComponent<Renderer>().material.color = Color.white;
-			lifeTimer -= Time.deltaTime;
-			if (lifeTimer <= 0)
-			{
-				lifeTimer = 999;	//bluh
-				coinSpawner.currentNumberOfCoins--;
-				Destroy(gameObject);
-			}
-		}
+
 	}
 
+	void OnMouseOver(){
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("clicked");
 
+			GetComponent<SpriteRenderer>().color = Color.white;
+			gameMaster.quarters++;
+			Destroy(gameObject);
+		}		
+	}	
 	
 }
