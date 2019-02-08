a6846744bef08a61462159c8008883c18cfe3781
Kristi Chang <kristi.chang@csu.fullerton.edu>
2019-02-07 19:13:07 -0800

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/CoinSilver.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSilver : MonoBehaviour
+{
+	
+	bool clicked;
+	public int coinValue;
+
+	public float minLifeLength;
+	public float maxLifeLength;
+	float lifeTimerLength;
+	float lifeTimer;
+
+	[HideInInspector]
+	public CoinSpawner coinSpawner;
+
+
+
+	void Start()
+	{
+		lifeTimerLength = Random.Range(minLifeLength, maxLifeLength);
+		lifeTimer = lifeTimerLength;
+
+		transform.localScale = new Vector2(0.2f, 0.2f);
+		GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+		
+		
+		GetComponent<Renderer>().material.color = Color.cyan;
+
+	}
+
+	void Update()
+	{
+		if (!clicked)
+		{
+			GetComponent<Renderer>().material.color = Color.white;
+			lifeTimer -= Time.deltaTime;
+			if (lifeTimer <= 0)
+			{
+				lifeTimer = 999;	//bluh
+				coinSpawner.currentNumberOfCoins--;
+				Destroy(gameObject);
+			}
+		}
+	}
+
+
+	
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	public void sceneHome(){
+		SceneManager.LoadScene("HomeScene");
+	}
+
+	public void sceneMap(){
+		SceneManager.LoadScene("MapScene");
+	}
+
+	public void sceneFountain(){
+		SceneManager.LoadScene("FountainScene");
+	}
+
+	public void sceneStore(){
+		SceneManager.LoadScene("StoreScene");
+	}
+
+}
