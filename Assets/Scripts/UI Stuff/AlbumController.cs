af86e28ac7b317abe8b5d670eac4a0c04ad71977
Kristi Chang <kristi.chang@csu.fullerton.edu>
2019-03-30 23:00:16 -0700

    non functional collection album

---
+++ Assets/Scripts/UI Stuff/AlbumController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlbumController : MonoBehaviour
+{
+	public GameObject[] pages;
+
+	public void openPage(int page)
+	{
+		for (int i = 0; i < pages.Length; i++)
+		{
+			if (i == page) {
+				pages[i].SetActive(true);
+			}
+			else {
+				pages[i].SetActive(false);
+			}
+		}
+	}
+
+	public void closeAll()
+	{
+		for (int i = 0; i < pages.Length; i++)
+		{
+			pages[i].SetActive(false);
+		}
+	}
+}
