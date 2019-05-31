5889c43a5db235e2457264edeb64b0a023e123e5
Michael Perez <mperez980089@yahoo.com>
2019-05-30 18:03:00 -0700

    Changed how removing the popup description works

---
+++ Assets/Scripts/UI Stuff/OnClickDisable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickDisable : MonoBehaviour
+{
+    public void TurnOff()
+    {
+        gameObject.SetActive(false);
+    }
+}
