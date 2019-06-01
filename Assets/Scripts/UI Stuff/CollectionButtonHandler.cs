d8d740b5e0575697b84cef0cb823566faa847bd4
Michael Perez <mperez980089@yahoo.com>
2019-05-31 18:09:15 -0700

    More collection scene refactors, improved item display overlay

--- Assets/Scripts/UI Stuff/CollectionButtonHandler.cs
+++ Assets/Scripts/UI Stuff/CollectionButtonHandler.cs
@@ -7,19 +7,32 @@ public class CollectionButtonHandler : MonoBehaviour
 {
     public int itemID;
     Item item;
-    public GameObject descriptionOverlay;
-    public Text descriptionText;
+
+    public GameObject displayOverlay;
+    Text displayName;
+    Text displayAmountOwned;
+    Image displayImage;
+    Text displayDescription;
 
     void Start()
     {
         //Decide if the button should be interactable or not
         item = GameMaster.instance.GetItem(itemID);
         if (item.amountOwned > 0) GetComponent<Button>().interactable = true;
+
+        //Get access to all display overlay elements
+        displayName = displayOverlay.transform.Find("Name").GetComponent<Text>();
+        displayAmountOwned = displayOverlay.transform.Find("Name/Amount Owned").GetComponent<Text>();
+        displayImage = displayOverlay.transform.Find("Image").GetComponent<Image>();
+        displayDescription = displayOverlay.transform.Find("Description").GetComponent<Text>();
     }
 
     public void DisplayItem()
     {
-        descriptionOverlay.SetActive(true);
-        descriptionText.text = item.itemName + ": " + item.amountOwned + "\n" + item.flavorText;
+        displayOverlay.SetActive(true);
+        displayName.text = item.itemName;
+        displayAmountOwned.text = "Amount owned: " + item.amountOwned;
+        displayImage.sprite = GameMaster.instance.itemImages[item.id];
+        displayDescription.text = item.flavorText;
     }
 }
